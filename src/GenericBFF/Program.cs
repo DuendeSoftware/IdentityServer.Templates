338df2c82db2b1a26f283040975710fa0707c763
Dominick Baier <dbaier@leastprivilege.com>
2023-02-11 11:29:34 +0100

    first cut

---
+++ src/GenericBFF/Program.cs
@@ -0,0 +1,69 @@
+using Duende.Bff.Yarp;
+using GenericBFF;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBff()
+    .AddRemoteApis();
+
+Configuration config = new();
+builder.Configuration.Bind("BFF", config);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "cookie";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("cookie", options =>
+    {
+        options.Cookie.Name = "__Host-bff";
+        options.Cookie.SameSite = SameSiteMode.Strict;
+    })
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = config.Authority;
+        options.ClientId = config.ClientId;
+        options.ClientSecret = config.ClientSecret;
+        
+        options.ResponseType = "code";
+        options.ResponseMode = "query";
+
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.MapInboundClaims = false;
+        options.SaveTokens = true;
+
+        options.Scope.Clear();
+        foreach (var scope in config.Scopes)
+        {
+            options.Scope.Add(scope);
+        }
+
+        options.TokenValidationParameters = new()
+        {
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+    });
+
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseBff();
+
+app.MapBffManagementEndpoints();
+
+if (config.Apis.Any())
+{
+    foreach (var api in config.Apis)
+    {
+        app.MapRemoteBffApiEndpoint(api.LocalPath, api.RemoteUrl!)
+            .RequireAccessToken(api.RequiredToken);
+    }
+}
+
+app.Run();
