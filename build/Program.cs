a60237ab4ce436f01c86d11ab07ef689d1703038
Brock Allen <brockallen@gmail.com>
2024-01-23 14:45:05 -0500

    update build project to .NET 8 and updated nuget version for this package

--- build/Program.cs
+++ build/Program.cs
@@ -7,7 +7,7 @@ namespace build
 {
     internal static class Program
     {
-        private const string NugetPackageVersion = "6.3.1";
+        private const string NugetPackageVersion = "7.0.0";
         
         private const string packOutput = "./artifacts";
         private const string envVarMissing = " environment variable is missing. Aborting.";
